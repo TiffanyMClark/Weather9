12f54e370df57d6dbb56fbe4b0f5ba165860d76f
tiffany clark <tiffanymarie9811@gmail.com>
2025-02-27 21:09:40 -0500

    changed server.ts going to wrong location to render

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,7 +14,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
-app.use(express.static(path.resolve(__dirname, "dist")));
+app.use(express.static(path.resolve(__dirname, "../client/dist")));
+
 // TODO: Implement middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
